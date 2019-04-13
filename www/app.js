a1ea32dca33cd632902889b4f7836e4e84956260
paulchin <paulchin20@yahoo.com>
2019-04-13 15:39:43 +0800

    'update'

--- www/app.js
+++ www/app.js
@@ -14,6 +14,9 @@ function initApp() {
       console.log('ajax panel error');
     });
   getHomeContent();
+
+  //--- Admob in ads.js ----
+  initAds();
 }
 
 function getHomeContent() {
@@ -32,56 +35,89 @@ function loadHomeContent() {
 
   window.scrollTo(0, 0);
   $('#top-title').html('PhoneGap AdMob');
+
+  //--- admob ---
+  showBannerAd();
 }
 
 function load1Content() {
   $('.ui-content').load('1.html');
   window.scrollTo(0, 0);
+
+  //--- admob ---
+  showInterstitialAd();
 }
 
 function load2Content() {
   $('.ui-content').load('2.html');
   window.scrollTo(0, 0);
+
+  //--- admob ---
+  showInterstitialAd();
 }
 
 function load3Content() {
   $('.ui-content').load('3.html');
   window.scrollTo(0, 0);
+
+  //--- admob ---
+  showInterstitialAd();
 }
 
 function load4Content() {
   $('.ui-content').load('4.html');
   window.scrollTo(0, 0);
+
+  //--- admob ---
+  showInterstitialAd();
 }
 
 function load5Content() {
   $('.ui-content').load('5.html');
   window.scrollTo(0, 0);
+
+  //--- admob ---
+  showRewardedVideoAd();
 }
 
 function load6Content() {
   $('.ui-content').load('6.html');
   window.scrollTo(0, 0);
+
+  //--- admob ---
+  showInterstitialAd();
 }
 
 function load7Content() {
   $('.ui-content').load('7.html');
   window.scrollTo(0, 0);
+
+  //--- admob ---
+  showInterstitialAd();
 }
 
 function load8Content() {
   $('.ui-content').load('8.html');
   window.scrollTo(0, 0);
+
+  //--- admob ---
+  showInterstitialAd();
 }
 
 function load9Content() {
   $('.ui-content').load('9.html');
   window.scrollTo(0, 0);
+
+  //--- admob ---
+  showInterstitialAd();
 }
 
 function load10Content() {
   $('.ui-content').load('10.html');
   window.scrollTo(0, 0);
+
+  //--- admob ---
+  showRewardedVideoAd();
 }
 
 function handleOptions() {
